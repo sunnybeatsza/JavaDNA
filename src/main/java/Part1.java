8ebae9c5da8a975a6443f882f7dc460f698900fb
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2025-01-07 01:27:15 +0200

    Part 1 - finding a simple gene

--- src/main/java/Part1.java
+++ src/main/java/Part1.java
@@ -1,2 +1,15 @@
 public class Part1 {
+    public String findSimpleGene(String dna){
+        int startCodonIndex = dna.indexOf("ATG");
+        int endCodonIndex = dna.indexOf("TAA", startCodonIndex + 3);
+        if (startCodonIndex == -1 || endCodonIndex == -1) {
+            return "";
+        }
+        String gene = dna.substring(startCodonIndex,endCodonIndex + 3);
+        if (gene.length() % 3 == 0){
+            return gene;
+        } else{
+            return "Invalid gene";
+        }
+    }
 }
