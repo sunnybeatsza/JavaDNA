24e110c99c8597111b9029212397663628777f7f
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2025-01-09 00:56:43 +0200

    changes

--- src/main/java/Part1.java
+++ src/main/java/Part1.java
@@ -1,15 +1,44 @@
+import java.util.ArrayList;
+
 public class Part1 {
-    public String findSimpleGene(String dna){
-        int startCodonIndex = dna.indexOf("ATG");
-        int endCodonIndex = dna.indexOf("TAA", startCodonIndex + 3);
-        if (startCodonIndex == -1 || endCodonIndex == -1) {
+
+    public static String findSimpleGene(String dna){
+        int startCodon = dna.indexOf("ATG");
+        if (startCodon == -1){
             return "";
         }
-        String gene = dna.substring(startCodonIndex,endCodonIndex + 3);
-        if (gene.length() % 3 == 0){
-            return gene;
-        } else{
-            return "Invalid gene";
+        int endCodon = dna.indexOf("TAA",startCodon + 3);
+        if (endCodon == -1){
+            return "";
+        }
+        String geneSubString = dna.substring(startCodon, endCodon + 3);
+        if (geneSubString.length() % 3 == 0){
+            return geneSubString;
+        } else {
+            return "";
         }
     }
+
+    public static void testfindSimpleGene(){
+        String dna1 = "CTGACGGTCTAAAG";
+        String dna2 = "ATGGCATGCGTCTG";
+        String dna3 = "GCGTAGGCATGACG";
+        String dna4 = "ATGGCTAAAGTAC";
+        String dna5 = "ATGGGGTAAAGT";
+        ArrayList<String> dnaList = new ArrayList<>();
+        dnaList.add(dna1);
+        dnaList.add(dna2);
+        dnaList.add(dna3);
+        dnaList.add(dna4);
+        dnaList.add(dna5);
+
+        for (String dna : dnaList){
+            System.out.println(findSimpleGene(dna));
+        }
+    }
+
+    public static void main(String[] args){
+        testfindSimpleGene();
+    }
+
 }
